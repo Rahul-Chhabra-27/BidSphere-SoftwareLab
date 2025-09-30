ffd4abf870cde64480bd110c9db5749fa80fbb9b
Rahul-Chhabra-27 <chhabrarahul2707@gmail.com>
2025-09-30 17:08:28 +0530

    checkout route added

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('/products',views.product,name='products_detail'),
     path('products/<int:id>/', views.product_detail, name='product_detail'),
+    #checkout to customers
+    path('/checkout',views.checkout,name='checkout-to-payment')
 ]
