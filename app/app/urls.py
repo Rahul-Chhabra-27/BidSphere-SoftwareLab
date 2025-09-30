838c29ffbb7fa4229cb73764ccb67fc5e1a32865
vedbanerjee <vedmumbai32@gmail.com>
2025-09-30 17:35:55 +0530

    Dashboard route added

--- app/app/urls.py
+++ app/app/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('products/<int:id>/', views.product_detail, name='product_detail'),
     #checkout to customers
     path('/checkout',views.checkout,name='checkout-to-payment')
+    #dashboard
+    path('/dashboard', views.dashboard, name = 'dashboard')
 ]
